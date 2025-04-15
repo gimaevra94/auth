0be60b0cd1ae7493e816785ad2acc33dbecd924b
gimaevra94 <gimaevra94@ya.ru>
2025-04-15 15:19:30 +0200

    checkpoint

--- app/logout/logout.go
+++ app/logout/logout.go
@@ -7,58 +7,35 @@ import (
 
 	"github.com/gimaevra94/auth/app/consts"
 	"github.com/gimaevra94/auth/app/structs"
-	"github.com/gorilla/sessions"
+	"github.com/golang-jwt/jwt"
 )
 
-/*func ActivityMiddleware(store *sessions.CookieStore) func(http.Handler) http.Handler {
+func IsExpiredTokenMW() func (http.Handler) http.Handler  {
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter,
-			r *http.Request) {
-			session, err := store.Get(r, "auth-session")
-			if err != nil || session.IsNew {
-				http.Redirect(w, r, "logout.html", http.StatusSeeOther)
-				return
-			}
+		return http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				cookie,err:=r.Cookie("Authorization")
+				if err!=nil{
+					http.ServeFile(w,r,"logout.html")
+					log.Println()
+					return
+				}
 
-			lastActivity, ok := session.Values["last_activity"].(lastActivity)
-			if !ok {
-				http.Redirect(w, r, "logout.html", http.StatusSeeOther)
-				return
-			}
+token:=cookie.Value
+if token==""{
+	http.ServeFile(w,r,consts.RequestErrorHTML)
+	return
+}
 
-			lastActivity.LastActivity = time.Now()
-			session.Values["last_activity"] = lastActivity
-			session.Save(r, w)
-
-			next.ServeHTTP(w, r)
-		})
-	}
-}*/
-
-func updateLastActivity(w http.ResponseWriter,
-	r *http.Request, store *sessions.CookieStore) bool {
-	session, err := store.Get(r, "auth-session")
-	if err != nil {
-		http.ServeFile(w, r, consts.RequestErrorHTML)
-		log.Println("Failed to get session")
-		return false
-	}
-
-	lastActivity, ok := session.Values["lastActivity"].(structs.LastActivity)
-	if !ok {
-		http.ServeFile(w, r, consts.RequestErrorHTML)
-		log.Println("Failed to get value 'lastActivity' from session")
-		return false
-	}
-
-	tokenExp := lastActivity.GetTokenExp()
-	if time.Now().After(tokenExp) {
-		activityExp := lastActivity.GetActivityExp()
-		if time.Since(activityExp) > 3*time.Hour {
-			delete(session.Values, "lastActivity")
-			session.Save(r, w)
-			http.Redirect()
-		}
-	}
+token,err:=jwt.Parse(token,GetJWTSecret)
+if err!=nil{
 
 }
+							}
+	)
+			}
+} {
+	
+}
+
+
