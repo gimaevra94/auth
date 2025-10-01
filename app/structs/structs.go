38ca6ec4a41830b679aa9c84c78e1b0940a9ada1
gimaevra94 <gimaevra94@ya.ru>
2025-10-01 10:27:09 +0200

    попросил бота актуализировать теги

--- app/structs/structs.go
+++ app/structs/structs.go
@@ -2,10 +2,10 @@ package structs
 
 type User struct {
 	UserID          string `sql:"userId"`
-	Login           string `sql:"login" json:"login"` //
-	Email           string `sql:"email" json:"email"` //
-	password        string `sql:"passwordHash"`       // Сделано неэкспортируемым и изменен тег sql
-	ServerCode      string // Тег sql удален, так как не используется в БД
+	Login           string `sql:"login" json:"login"`
+	Email           string `sql:"email" json:"email"`
+	Password        string `sql:"passwordHash"`
+	ServerCode      string
 	TemporaryUserID string `sql:"temporaryUserID"`
 	PermanentUserID string `sql:"permanentUserID"`
 }
