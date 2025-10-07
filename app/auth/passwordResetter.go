d8c400f26b7a9df839b4e0e44db72e62d26c6896
gimaevra94 <gimaevra94@ya.ru>
2025-10-07 09:28:06 +0200

    Просто переменовал

---
+++ app/auth/passwordResetter.go
@@ -0,0 +1,125 @@
+package auth
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gimaevra94/auth/app/consts"
+	"github.com/gimaevra94/auth/app/data"
+	"github.com/gimaevra94/auth/app/tools"
+	"github.com/pkg/errors"
+)
+
+func PasswordResetEmailCheck(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	err := tools.EmailValidate(email)
+	if err != nil {
+		err := tools.TmplsRenderer(w, tools.BaseTmpl, "PasswordReset", struct{ Msg string }{Msg: tools.ErrMsg["email"].Msg})
+		if err != nil {
+			log.Printf("%+v", err)
+			http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+			return
+		}
+		return
+	}
+
+	err = data.PasswordResetEmailCheck(email)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err := tools.TmplsRenderer(w, tools.BaseTmpl, "PasswordReset", struct{ Msg string }{Msg: tools.ErrMsg["notExist"].Msg})
+			if err != nil {
+				log.Printf("%+v", errors.WithStack(err))
+				http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+				return
+			}
+			return
+		}
+
+		log.Printf("%+v", errors.WithStack(err))
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+
+	baseURL := "http://localhost:8080/set-new-password"
+	resetLink, err := tools.GenerateResetLink(email, baseURL)
+	if err != nil {
+		log.Printf("%+v", errors.WithStack(err))
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+
+	err = tools.SendPasswordResetEmail(email, resetLink)
+	if err != nil {
+		log.Printf("%+v", errors.WithStack(err))
+		err := tools.TmplsRenderer(w, tools.BaseTmpl, "PasswordReset", struct{ Msg string }{Msg: tools.MailSendingStatusMsg})
+		if err != nil {
+			log.Printf("%+v", errors.WithStack(err))
+			http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+			return
+		}
+	}
+}
+
+func SetNewPassword(w http.ResponseWriter, r *http.Request) {
+	signedToken := r.URL.Query().Get("token")
+	if signedToken == "" {
+		log.Println("reset-token not exist")
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+
+	claims, err := tools.ValidateResetToken(signedToken)
+	if err != nil {
+		log.Printf("%+v", errors.WithStack(err))
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+
+	cancelled, err := data.ResetTokenCheck(signedToken)
+	if err != nil {
+		log.Printf("%+v", errors.WithStack(err))
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+	if cancelled {
+		err := errors.New("reset-token invalid")
+		log.Printf("%+v", errors.WithStack(err))
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+
+	newPassword := r.FormValue("newPassword")
+	confirmPassword := r.FormValue("confirmPassword")
+
+	if newPassword != confirmPassword {
+		log.Println("New password validation failed")
+		err := tools.TmplsRenderer(w, tools.BaseTmpl, "SetNewPassword", struct{ Msg string }{Msg: tools.ErrMsg["password"].Msg})
+		if err != nil {
+			log.Printf("%+v", errors.WithStack(err))
+			http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+			return
+		}
+
+		err = tools.PasswordValidate(newPassword)
+		if err != nil {
+			log.Printf("%+v", errors.WithStack(err))
+			http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+			return
+		}
+	}
+
+	err = data.UpdatePassword(claims.Email, newPassword)
+	if err != nil {
+		log.Printf("%+v", errors.WithStack(err))
+		http.Redirect(w, r, consts.Err500URL, http.StatusFound)
+		return
+	}
+
+	err = data.ResetTokenCancel(signedToken)
+	if err != nil {
+		log.Printf("%+v", errors.WithStack(err))
+	}
+
+	http.Redirect(w, r, consts.SignInURL+"?msg=PasswordSuccessfullyReset", http.StatusFound)
+}
