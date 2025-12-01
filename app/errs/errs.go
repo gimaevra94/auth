61bafce79e15f3984d04d55d6746dc94d7af967f
gimaevra94 <gimaevra94@ya.ru>
2025-12-01 10:38:33 +0200

    Написана документация для errs.go

--- app/errs/errs.go
+++ app/errs/errs.go
@@ -1,3 +1,7 @@
+// Package errs предоставляет утилиты для обработки ошибок.
+//
+// Файл содержит функции для логирования и перенаправления при ошибках:
+//   - LogAndRedirectIfErrNotNill: логирует ошибку и выполняет перенаправление
 package errs
 
 import (
@@ -7,6 +11,10 @@ import (
 	"github.com/gimaevra94/auth/app/consts"
 )
 
+// LogAndRedirectIfErrNotNill обрабатывает ошибку, логирует её и выполняет перенаправление.
+//
+// Логирует ошибку с полным стеком вызовов и перенаправляет пользователя на указанный URL.
+// Если URL пустой или равен "/", перенаправляет на страницу ошибки 500.
 func LogAndRedirectIfErrNotNill(w http.ResponseWriter, r *http.Request, err error, url string) {
 	log.Printf("%+v", err)
 	if url == "" || url == "/" {
